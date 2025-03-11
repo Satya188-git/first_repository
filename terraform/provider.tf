fb70ce074e25843c0e224d5826bbdde442fbc2fe
Satya188-git <satyaponnada613@gmail.com>
2025-03-11 22:57:08 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "us-east-1" # Change to your preferred region
-  AWS_ACCESS_KEY_ID = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  AWS_SECRET_ACCESS_KEY = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  
           
 }
